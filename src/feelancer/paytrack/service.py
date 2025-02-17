52e5c2168ee655787f117bebcf94536408ef2efd
feelancer21 <2828397+feelancer21@users.noreply.github.com>
2025-02-18 00:40:42 +0100

    paytrack: add data handling
    first working prototype

--- src/feelancer/paytrack/service.py
+++ src/feelancer/paytrack/service.py
@@ -1,8 +1,7 @@
-import hashlib
-import logging
 import threading
 
 from feelancer.data.db import FeelancerDB
+from feelancer.lightning.data import LightningStore
 
 from .data import PaymentTrackerStore
 from .tracker import PaymentTracker
@@ -23,6 +22,7 @@ class PaytrackService:
     ) -> None:
 
         self.store = PaymentTrackerStore(db)
+        self.ln_store = LightningStore(db, payment_tracker.pubkey_local)
         self.payment_tracker = payment_tracker
         self.paytrack_config = paytrack_config
         self.is_stopped: bool = False
@@ -32,18 +32,8 @@ class PaytrackService:
         thread = threading.Thread(target=self.payment_tracker.start)
         thread.start()
 
-        for a in self.payment_tracker.generate_attempts():
-            hashsum = hashlib.sha256(
-                a.SerializePartialToString(deterministic=True)
-            ).hexdigest()
-
-            hashsum_route = hashlib.sha256(
-                a.route.SerializePartialToString(deterministic=True)
-            ).hexdigest()
-
-            logging.debug(
-                f"attempt: {a.attempt_id}, status: {a.status}, attempt_time: {a.attempt_time_ns}, resolve_time_ns: {a.resolve_time_ns}, failure: {a.failure.code}, hashsum: {hashsum}, hashsum_route: {hashsum_route}"
-            )
+        gen_attempts = self.payment_tracker.generate_attempts(self.ln_store.ln_node_id)
+        self.store.store_attempts(gen_attempts)
 
         thread.join()
 
