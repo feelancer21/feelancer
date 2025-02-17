ae04e4bd85359ef5f7f5d3932903891618be6529
feelancer21 <2828397+feelancer21@users.noreply.github.com>
2025-02-17 17:43:26 +0100

    paytrack: add basic tracker und service

--- src/feelancer/paytrack/service.py
+++ src/feelancer/paytrack/service.py
@@ -1,5 +1,6 @@
+import hashlib
 import logging
-import time
+import threading
 
 from feelancer.data.db import FeelancerDB
 
@@ -27,11 +28,23 @@ class PaytrackService:
 
     def start(self) -> None:
 
-        self.payment_tracker.start()
+        thread = threading.Thread(target=self.payment_tracker.start)
+        thread.start()
 
-        while not self.is_stopped:
-            logging.debug("Paytrack Service running")
-            time.sleep(1)
+        for a in self.payment_tracker.generate_attempts():
+            hashsum = hashlib.sha256(
+                a.SerializePartialToString(deterministic=True)
+            ).hexdigest()
+
+            hashsum_route = hashlib.sha256(
+                a.route.SerializePartialToString(deterministic=True)
+            ).hexdigest()
+
+            logging.debug(
+                f"attempt: {a.attempt_id}, status: {a.status}, attempt_time: {a.attempt_time_ns}, resolve_time_ns: {a.resolve_time_ns}, failure: {a.failure.code}, hashsum: {hashsum}, hashsum_route: {hashsum_route}"
+            )
+
+        thread.join()
 
     def stop(self) -> None:
 
--- src/feelancer/paytrack/tracker.py
+++ src/feelancer/paytrack/tracker.py
@@ -1,25 +1,41 @@
-import logging
 from collections.abc import Generator
 from typing import Protocol
 
-from feelancer.lightning.lnd import LNDClient
+from feelancer.grpc.client import GrpcStreamClient
+from feelancer.lnd.client import LndGrpc
+from feelancer.lnd.grpc_generated import lightning_pb2 as ln
 
 
 class PaymentTracker(Protocol):
 
-    def generate_attempts(self) -> Generator: ...
-
-    def start(self) -> None: ...
-
-    def stop(self) -> None: ...
-
-
-class LNDPaymentTracker(LNDClient):
-
-    def generate_attempts(self) -> Generator: ...
+    def generate_attempts(self) -> Generator[ln.HTLCAttempt]: ...
 
     def start(self) -> None:
-        logging.debug("Paytrack start")
+        """
+        Starts the payment tracker.
+        """
+        ...
 
     def stop(self) -> None:
-        logging.debug("Paytrack stop")
+        """
+        Stops the payment tracker.
+        """
+        ...
+
+
+class LNDPaymentTracker(GrpcStreamClient[ln.Payment]):
+
+    def __init__(self, lnd: LndGrpc):
+
+        super().__init__(name="LndPaymentTracker", producer=lnd.track_payments)
+
+    def generate_attempts(self) -> Generator[ln.HTLCAttempt]:
+
+        for p in self.generate_messages():
+            # we only process status SUCCEEDED or FAILED
+            if p.status not in [2, 3]:
+                continue
+
+            for h in p.htlcs:
+
+                yield h
