64b5e1a7ee67fb8433da55b96d7e37b279ff685b
feelancer21 <2828397+feelancer21@users.noreply.github.com>
2025-03-23 19:59:15 +0100

    multi: refactor stores

--- src/feelancer/pid/service.py
+++ src/feelancer/pid/service.py
@@ -4,14 +4,14 @@ import logging
 from collections.abc import Callable
 from typing import TYPE_CHECKING
 
-from feelancer.lightning.data import LightningStore
 from feelancer.tasks.runner import RunnerRequest, RunnerResult
 
 from .controller import PidController
 from .data import PidStore
 
 if TYPE_CHECKING:
-    from feelancer.data.db import FeelancerDB
+    # from feelancer.data.db import FeelancerDB
+    from feelancer.lightning.data import LightningStore
 
     from .data import PidConfig
 
@@ -22,36 +22,36 @@ class PidService:
 
     def __init__(
         self,
-        db: FeelancerDB,
-        pubkey_local: str,
+        ln_store: LightningStore,
+        pid_store: PidStore,
         get_pid_config: Callable[..., PidConfig | None],
     ):
 
-        self.pid_store = PidStore(db, pubkey_local)
-        self.ln_store = LightningStore(db, pubkey_local)
-        self.get_pid_config: Callable[..., PidConfig | None] = get_pid_config
-        self.pid_controller: PidController | None = None
+        self._ln_store = ln_store
+        self._pid_store = pid_store
+        self._get_pid_config: Callable[..., PidConfig | None] = get_pid_config
+        self._pid_controller: PidController | None = None
 
     def run(self, request: RunnerRequest) -> RunnerResult:
         """
         Runs the the pid model.
         """
 
-        pid_config = self.get_pid_config()
+        pid_config = self._get_pid_config()
         if pid_config is None:
             return RunnerResult(None, None)
 
         logger.info("Running pid controller...")
 
-        if not self.pid_controller:
-            self.pid_controller = PidController(
-                self.pid_store, self.ln_store, pid_config
+        if not self._pid_controller:
+            self._pid_controller = PidController(
+                self._pid_store, self._ln_store, pid_config
             )
 
-        self.pid_controller(pid_config, request.ln, request.timestamp)
+        self._pid_controller(pid_config, request.ln, request.timestamp)
 
         res = RunnerResult(
-            self.pid_controller.store_data, self.pid_controller.policy_proposals()
+            self._pid_controller.store_data, self._pid_controller.policy_proposals()
         )
 
         logger.info("Finished pid controller")
@@ -62,5 +62,5 @@ class PidService:
         Resets the subserver
         """
 
-        self.pid_controller = None
+        self._pid_controller = None
         logger.debug("Finished reset of pid controller")
