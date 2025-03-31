fa9235cd37858e959ba192e840a885c52a6a1b69
feelancer21 <2828397+feelancer21@users.noreply.github.com>
2025-03-31 09:48:36 +0200

    tracker: rename forwards to htlcs

--- src/feelancer/tracker/htlcs/service.py
+++ src/feelancer/tracker/htlcs/service.py
@@ -2,11 +2,11 @@ from feelancer.tasks.runner import RunnerRequest, RunnerResult
 from feelancer.tracker.proto import TrackerBaseService
 
 
-class FwdtrackConfig:
+class HtlctrackConfig:
     def __init__(self, config_dict: dict) -> None: ...
 
 
-class FwdtrackService(TrackerBaseService[FwdtrackConfig]):
+class HtlctrackService(TrackerBaseService[HtlctrackConfig]):
 
     def run(self, request: RunnerRequest) -> RunnerResult:
         return RunnerResult()
